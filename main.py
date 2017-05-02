b694c73d038b8fee815c751bffd29074266158ff
Kasama <ralegro@bol.com.br>
2017-05-02 14:10:44 -0300

    fixed tray click

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def xdotool(command, id):
     return ["xdotool", command, str(id)]
 
 
-class SpotifyWrapper(QtGui.QSystemTrayIcon):
-    def __init__(self, spotify_id, parent=None):
+class SpotifyWrapper(QtGui.QSystemTrayIcon, subprocess.Popen):
+    def __init__(self, spotify_id, spotify_process, parent=None):
         self.app = QtGui.QApplication(sys.argv)
         self.spotify_id = spotify_id
-        # TODO: Find more reliable way of keeping track of spotify visibility status
+        self.spotify_process = spotify_process
+        # TODO: Find reliable way of keeping track of spotify visibility status
         self.spotify_hidden = False
 
         # TODO: Change to spotify icon
@@ -28,25 +29,41 @@ class SpotifyWrapper(QtGui.QSystemTrayIcon):
         QtGui.QSystemTrayIcon.__init__(self, icon, parent)
 
         self.menu = QtGui.QMenu(parent)
-        QtCore.QObject.connect(self.menu, QtCore.SIGNAL('aboutToShow()'), self.populate_menu)
-        QtCore.QObject.connect(self, QtCore.SIGNAL('activated(QSystemTrayIcon::ActivationReason)'), self.icon_click)
+        QtCore.QObject.connect(
+                self.menu,
+                QtCore.SIGNAL('aboutToShow()'),
+                self.populate_menu
+                )
+        QtCore.QObject.connect(
+                self,
+                QtCore.SIGNAL('activated(QSystemTrayIcon::ActivationReason)'),
+                self.icon_click
+                )
         self.setContextMenu(self.menu)
 
     def icon_click(self, reason):
-        self.toggle_spotify_visibility()
+        if reason == QtGui.QSystemTrayIcon.Trigger:
+            self.toggle_spotify_visibility()
 
     def populate_menu(self):
         self.menu.clear()
         playing_now = self.track_info()
-        track_info = ', '.join(playing_now['artist']) + " - " + playing_now['title'] + "\nfrom: " + playing_now['album']
-        self.menu.addAction(track_info, lambda: self.toggle_spotify_visibility())
+        track_info = ', '.join(playing_now['artist']) + \
+            " - " + playing_now['title'] + \
+            "\nfrom: " + playing_now['album']
+
+        self.menu.addAction(
+                track_info,
+                lambda: self.toggle_spotify_visibility()
+                )
 
         self.menu.addSeparator()
         self.menu.addAction('Show Spotify', lambda: self.show_spotify())
         self.menu.addAction('Hide Spotify', lambda: self.hide_spotify())
 
         self.menu.addSeparator()
-        self.menu.addAction('Pause' if self.isPlaying() else 'Play', lambda: self.play_pause())
+        play_pause_text = 'Pause' if self.isPlaying() else 'Play'
+        self.menu.addAction(play_pause_text, lambda: self.play_pause())
         self.menu.addAction('Next', lambda: self.next_track())
         self.menu.addAction('Previous', lambda: self.prev_track())
 
@@ -92,8 +109,10 @@ class SpotifyWrapper(QtGui.QSystemTrayIcon):
         val = subprocess.check_output(playerctl("metadata")).decode("utf-8")
         val = val.replace("<", "").replace(">", "").replace("uint64 ", "")
         val = val.replace("xesam:", "").replace("mpris:", "")
-        # Evaluating literally because of single/double quotes problem "this 'string', for an example"
+        # Evaluating literally because of single/double quotes problem
+        # eg. {'something' : "this 'string', for an example"}
         data = ast.literal_eval(val)
+        data['playing'] = self.isPlaying()
         # data = json.loads(val)
         return data
 
@@ -110,7 +129,7 @@ def main():
         find_failed = subprocess.call(find_spotify_command)
     spotify_id = int(subprocess.check_output(find_spotify_command))
 
-    spotify = SpotifyWrapper(spotify_id)
+    spotify = SpotifyWrapper(spotify_id, spotify_process)
     spotify.show()
     sys.exit(spotify.app.exec_())
 
