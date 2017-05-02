dde45e1175920fd89f56354f70533e1067573a6d
Kasama <ralegro@bol.com.br>
2017-05-01 23:47:12 -0300

    fixed some options and added icon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import ast
 import signal
 from PyQt4 import QtGui, QtCore
 import json
@@ -19,23 +20,32 @@ class SpotifyWrapper(QtGui.QSystemTrayIcon):
         self.spotify_hidden = False
 
         # TODO: Change to spotify icon
-        icon = QtGui.QIcon(style.standardPixmap(QtGui.QStyle.SP_FileIcon))
+        #icon = QtGui.QIcon(style.standardPixmap(QtGui.QStyle.SP_FileIcon))
+        icon = QtGui.QIcon("./spotify.png")
         QtGui.QSystemTrayIcon.__init__(self, icon, parent)
 
         self.menu = QtGui.QMenu(parent)
         QtCore.QObject.connect(self.menu, QtCore.SIGNAL('aboutToShow()'), self.populate_menu)
+        QtCore.QObject.connect(self, QtCore.SIGNAL('activated(QSystemTrayIcon::ActivationReason)'), self.icon_click)
         self.setContextMenu(self.menu)
+
+    def icon_click(self, reason):
+        self.toggle_spotify_visibility()
 
     def populate_menu(self):
         self.menu.clear()
         playing_now = self.track_info()
         track_info = ', '.join(playing_now['artist']) + " - " + playing_now['title'] + "\nfrom: " + playing_now['album']
-        self.menu.addAction(track_info, self.toggle_spotify_visibility)
+        self.menu.addAction(track_info, lambda: self.toggle_spotify_visibility())
+
+        self.menu.addSeparator()
+        self.menu.addAction('Show Spotify', lambda: self.show_spotify())
+        self.menu.addAction('Hide Spotify', lambda: self.hide_spotify())
 
         self.menu.addSeparator()
         self.menu.addAction('Pause' if self.isPlaying() else 'Play', lambda: self.play_pause())
         self.menu.addAction('Next', lambda: self.next_track())
-        self.menu.addAction('Previous', lambda: self.previous_track())
+        self.menu.addAction('Previous', lambda: self.prev_track())
 
         self.menu.addSeparator()
         self.menu.addAction("Exit", lambda: self.exit_pressed())
@@ -53,6 +63,7 @@ class SpotifyWrapper(QtGui.QSystemTrayIcon):
 
     def show_spotify(self):
         subprocess.call(xdotool("windowmap", self.spotify_id))
+        subprocess.call(xdotool("windowactivate", self.spotify_id))
 
     def play_pause(self):
         subprocess.call(playerctl("play-pause"))
@@ -76,9 +87,11 @@ class SpotifyWrapper(QtGui.QSystemTrayIcon):
 
     def track_info(self):
         val = subprocess.check_output(playerctl("metadata")).decode("utf-8")
-        val = val.replace("<", "").replace(">", "").replace("'", "\"").replace("uint64 ", "")
+        val = val.replace("<", "").replace(">", "").replace("uint64 ", "")
         val = val.replace("xesam:", "").replace("mpris:", "")
-        data = json.loads(val)
+        # Evaluating literally because of single/double quotes problem "this 'string', for an example"
+        data = ast.literal_eval(val)
+        #data = json.loads(val)
         return data
 
 def main():
@@ -92,7 +105,6 @@ def main():
     while find_failed == 1:
         find_failed = subprocess.call(find_spotify_command)
     spotify_id = int(subprocess.check_output(find_spotify_command))
-    print("spotify id: " + str(spotify_id))
 
     spotify = SpotifyWrapper(spotify_id)
     spotify.show()
